2786998df60255962e52bc53f3b1eced06fb7ee6
JaoodxD <jaood96@gmail.com>
2023-12-04 12:04:28 +0200

    Add test case to update product title

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ import PromUa from './lib/prom-ua.js'
 
 const PROM_API_KEY = process.env.PROM_API_KEY
 
-const { getProducts } = PromUa(PROM_API_KEY)
+const { getProducts, updateProducts } = PromUa(PROM_API_KEY)
 
 console.time('products')
-const products = await getProducts()
+const data = await getProducts()
 console.timeEnd('products')
 
-console.log(products.products.map(({ id, name, sku }) => ({ id, name, sku })))
+const products = data.products.map(({ id, name, sku }) => ({ id, name, sku }))
+const testProducts = products.filter(({ sku }) => sku.includes('test'))
+
+const payload = testProducts.map(product => {
+  const data = { ...product }
+  data.name += '#'
+  return data
+})
+
+console.log(payload)
+
+const updateResult = await updateProducts(payload)
+console.log(updateResult)
